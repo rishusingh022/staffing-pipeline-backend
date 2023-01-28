dfd73804a9aae818bf25adf3edabdbd4b8aa5d48
Sukhacoder02 <sukhmansindhipb05@gmail.com>
2023-01-28 15:03:31 +0530

    feat: implement GET practitioners Request

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 //create an express server 
 const express = require('express');
+const router = require('./routes/practitionerRoutes');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-//pre requisites
+// middlewares
 app.use(express.json());
+app.use(router);
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World!'); 
+  res.send('Hello World!');
 });
+
+
 
 //start a server on port 3000
 app.listen(PORT, () => {
