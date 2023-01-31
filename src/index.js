ead5f8caf5c2da595be7bb1ab78db7ef7f1c7760
Promit-revar <promit.revar2211@gmail.com>
2023-01-31 22:22:37 +0530

    chore: Add required dependencies and setup swagger documentation for the API

--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@
 const express = require('express');
 const router = require('./routes/practitionerRoutes');
 
+//add swagger documentation
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger.json');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // middlewares
 app.use(express.json());
 app.use(router);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 
 app.get('/', (req, res) => {
