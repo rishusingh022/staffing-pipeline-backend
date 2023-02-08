a1285bfc363bda0114ca4a0cbebe86af88d1be4c
Sneha <snehalall.sneha@gmail.com>
2023-02-08 10:15:48 +0530

    feat : add routes for project

---
+++ src/routes/projectRoutes.js
@@ -0,0 +1,17 @@
+const express=require('express');
+const {getProject,listProjects,createProject,deleteProject,updateProject}=require('./../controllers/projectController');
+
+
+const projectRouter=express.Router();
+
+
+projectRouter.route('/')
+  .get(listProjects)
+  .post(createProject);
+
+projectRouter.route('/:id')
+  .get(getProject)
+  .patch(updateProject)
+  .delete(deleteProject);
+
+module.exports=projectRouter;
