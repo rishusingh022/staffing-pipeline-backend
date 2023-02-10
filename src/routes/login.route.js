6f1a8e79e4842632192c67acc1740fce1ca29775
khushilms-cs19 <67272210+khushilms-cs19@users.noreply.github.com>
2023-02-10 16:32:50 +0530

    fix: fix import (#20)
    * feat: create delete endpoint and tests
    
    * chore: apply file naming convention
    
    * chore: apply file naming convention for users
    
    * chore: add file naming convention for users controller, services and routes
    
    * chore: file naming convention for users
    
    * chore: file naming convention for users
    
    * fix: database config file for test
    
    * chore: formatting with prettier
    
    * chore: format files and merging
    
    * chore: new prettier config
    
    * chore: add plugin eslint in prettier
    
    * chore: file naming change based on convention
    
    * chore: change case study filename
    
    * chore: resolve imports

--- src/routes/login.route.js
+++ src/routes/login.route.js
@@ -1,6 +1,6 @@
 const authRouter = require('express').Router();
 const { loginController } = require('../controllers/auth.controller');
-const { validateLoginReq } = require('../middlewares/login.validation');
+const { validateLoginReq } = require('../middlewares/login.validator');
 
 authRouter.post('/login', validateLoginReq, loginController);
 module.exports = authRouter;
